444a900bf8366d759d480f599a16708533b453b3
이상남 <lsn5963@naver.com>
2023-06-20 00:40:51 +0900

    init

---
+++ learn-spring-framework-01/src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,23 @@
+package com.in28minutes.learnspringframework.game;
+
+public class PacmanGame implements GamingConsole{
+    @Override
+    public void up() {
+        System.out.println("up");
+    }
+
+    @Override
+    public void down() {
+
+    }
+
+    @Override
+    public void left() {
+
+    }
+
+    @Override
+    public void right() {
+
+    }
+}
