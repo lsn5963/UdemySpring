b524017eb89aadb63d926f8019c3f478534e6e23
이상남 <lsn5963@naver.com>
2023-06-29 19:47:33 +0900

    feat: 6단계 - Spring JDBC를 사용하여 데이터 삽입 및 삭제하기

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/Course.java
@@ -0,0 +1,37 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course;
+
+public class Course {
+    private int id;
+    private String name;
+    private String author;
+
+    public Course() {
+    }
+
+    public Course(int id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,10 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     private CourseJdbcRepository repository;
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, "Learn AWS Now1!", "in28minutes"));
+        repository.insert(new Course(2, "Learn AWS Now2!", "in28minutes"));
+        repository.insert(new Course(3, "Learn AWS Now3!", "in28minutes"));
+
+        repository.deleteById(1);
     }
 }
