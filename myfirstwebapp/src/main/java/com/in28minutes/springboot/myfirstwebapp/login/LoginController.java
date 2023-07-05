c8365ea29ea8ab974dfa6879a1daf86aeb30b66f
이상남 <lsn5963@naver.com>
2023-07-06 00:12:23 +0900

    feat: 9단계 - 빠른 개요 - Spring Boot를 사용할 때 로깅의 중요성

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -5,13 +5,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class LoginController {
+//    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("login")
     public String gotoLoginPage(@RequestParam String name, ModelMap model){
         model.put("name", name);
-        System.out.println("name = " + name);
         return "login";
     }
 }
