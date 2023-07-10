94dfe955659166e54dfbb682e28872992f4b181c
이상남 <lsn5963@naver.com>
2023-07-11 02:34:59 +0900

    feat: 15단계 - Todo 리스트 페이지 처음 만들기

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -1,16 +1,20 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class TodoService {
-    private static List<Todo> todos;
+    private static List<Todo> todos = new ArrayList<>();
     static {
         todos.add(new Todo(1,"in28minutes", "Learn AWS", LocalDate.now().plusDays(1), false));
         todos.add(new Todo(2,"in28minutes", "Learn DevOps", LocalDate.now().plusDays(2), false));
         todos.add(new Todo(3,"in28minutes", "Learn Full Stack Development", LocalDate.now().plusDays(3), false));
     }
-    private List<Todo> findByUsername(String username){
+    public List<Todo> findByUsername(String username){
         return todos;
     }
 }
