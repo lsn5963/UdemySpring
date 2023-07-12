dbaa844dc54410949bf3ca4a664eefa28a033ee8
이상남 <lsn5963@naver.com>
2023-07-13 01:40:27 +0900

    feat: 23단계 - 커맨드 빈으로 새 Todo 페이지 검증 구현하기

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -1,11 +1,14 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 
 public class Todo {
     private int id;
     private String username;
+    @Size(min=10, message = "least 10 character")
     private String description;
     private LocalDate targetDate;
     private boolean done;
