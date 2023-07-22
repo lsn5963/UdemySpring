b68744990370b8944b28d4cc689671553ebc29b3
이상남 <lsn5963@naver.com>
2023-07-22 23:59:59 +0900

    feat: 36단계 - Todo 엔터티를 만들고 Todo 데이터를 H2에 채워넣기

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -1,11 +1,18 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
 
+@Entity
 public class Todo {
+    @Id
+    @GeneratedValue
     private int id;
     private String username;
     @Size(min=10, message = "least 10 character")
