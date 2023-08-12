82eb4a7851112a55eeaa690ffd3f948c01acbdb8
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2023-08-12 17:13:51 +0900

    feat: 13단계 - DELETE 메소드로 사용자 리소스 삭제하기

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/ErrorDetails.java
@@ -0,0 +1,18 @@
+package com.in28minutes.rest.webservices.restfulwebservices.exception;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+}
