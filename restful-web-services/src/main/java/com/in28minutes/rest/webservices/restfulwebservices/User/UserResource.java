a28e3e74ced16b27d2ac53c5d69ac93469b8aaa2
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2023-09-01 17:33:37 +0900

    feat: 21단계 - REST API 버전 관리 - 요청 매개변수, 헤더, 콘텐츠 협상

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/User/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/User/UserResource.java
@@ -1,6 +1,9 @@
 package com.in28minutes.rest.webservices.restfulwebservices.User;
 
 import jakarta.validation.Valid;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,17 +26,33 @@ public class UserResource {
         return service.findAll();
     }
 
+
+//    // GET /users
+//    @GetMapping("/users/{id}")
+//    public User retrieveUser(@PathVariable Integer id) {
+//        User user = service.findOne(id);
+//
+//        if(user == null){
+//            throw new UserNotFoundException("id:"+id);
+//        }
+//        return user;
+//    }
+
     // GET /users
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable Integer id) {
+    public EntityModel<User> retrieveUser(@PathVariable Integer id) {
         User user = service.findOne(id);
 
         if(user == null){
             throw new UserNotFoundException("id:"+id);
         }
-        return user;
-    }
+        EntityModel<User> entityModel = EntityModel.of(user);
 
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
+
+    }
     //POST /users
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
