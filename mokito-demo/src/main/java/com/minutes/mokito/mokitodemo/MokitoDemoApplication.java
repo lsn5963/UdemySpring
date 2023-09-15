764857f62e8ab499e1e19162cb3a8528f09378e6
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2023-09-16 00:58:16 +0900

    1단계 - Spring Boot 프로젝트 설정하기

---
+++ mokito-demo/src/main/java/com/minutes/mokito/mokitodemo/MokitoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.minutes.mokito.mokitodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokitoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokitoDemoApplication.class, args);
+	}
+
+}
