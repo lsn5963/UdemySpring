9180d55719ae76a7f267faa5469e344173704b3b
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2023-09-19 23:55:08 +0900

    4단계 - Mockito 어노테이션(@Mock, @InjectMocks)을 이용헤 테스트 단순화 하기

--- mokito-demo/src/test/java/com/minutes/mokito/mokitodemo/business/SomeBusinessImpMocklTest.java
+++ mokito-demo/src/test/java/com/minutes/mokito/mokitodemo/business/SomeBusinessImpMocklTest.java
@@ -1,18 +1,25 @@
 package com.minutes.mokito.mokitodemo.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessImplMockTest {
+    @Mock
+    private DataService dataServiceMock;
+    @InjectMocks
+    private SomeBusinessImpl businessImpl;
     @Test
     void findTheGreatestFromAllData_basicScenario(){
-        DataService dataServiceMock = mock(DataService.class);
 //        dataServiceMock.retrieveAllData() => new int[]{25, 15, 5};
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{25, 15, 5});
-        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
         assertEquals(25, businessImpl.findTheGreatestFromAllData());
     }
 }
