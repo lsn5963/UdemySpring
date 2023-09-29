c069a60840f3ec83fe1a2003d9b9e775a06e8e21
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2023-09-30 00:14:50 +0900

    15단계 - Spring Security 살펴보기 - Bcrypt 인코딩 암호 저장하기

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,13 +2,20 @@ package com.in28minutes.learnspringsecurity.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -31,23 +38,56 @@ public class BasicAuthSecurityConfiguration {
 
 
         http.httpBasic(withDefaults());
+        http.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()));
 
         http.csrf(csrf -> csrf.disable());// csrf 해제
 
         return http.build();
     }
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        var user = User.withUsername("1111")
+//                .password("{noop}1111")
+//                .roles("USER")
+//                .build();
+//        var admin = User.withUsername("admin")
+//                .password("{noop}dummy")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+
     @Bean
-    public UserDetailsService userDetailsService(){
-        var user = User.withUsername("user")
-                .password("{noop}dummy")
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        var user = User.withUsername("1111")
+//                .password("{noop}1111")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
                 .roles("USER")
                 .build();
         var admin = User.withUsername("admin")
-                .password("{noop}dummy")
+//                .password("{noop}dummy")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user, admin);
+
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+        return jdbcUserDetailsManager;
     }
 
-
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
